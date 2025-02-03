582bb384d3807ed078a14b4160ff3cdccffe9f5b
eliastrana <eliastrana@gmail.com>
2025-02-03 17:03:57 +0100

    feat(frontend): Exampleprompts on frontpage :sparkles:
    Updated the questions and fixed mobile layout.

--- app/components/chat_interface/ExamplePrompts.tsx
+++ app/components/chat_interface/ExamplePrompts.tsx
@@ -7,19 +7,19 @@ interface ExamplePromptsProps {
 
 export default function ExamplePrompts({ onSelectPrompt }: ExamplePromptsProps) {
     const prompts = [
-        "Hva var inflasjonsraten i Norge fra 2007-2011?",
-        "Hvor mye tjente folk i gjennomsnitt fra 2010-2020?",
-        "Hvor mange folk var det i norge fra 2000-2010 per år?"
+        "Hvor mye CO₂ slipper Norge ut per innbygger?",
+        "Hvor mye tjente kvinner fra 2000-2010?",
+        "Hva var Norges BNP fra 2010-2020?"
     ];
 
     return (
         <div className="flex flex-col items-center mt-4">
-            <h2 className="text-xl font-semibold text-gray-800">Vi kan hjelpe deg med:</h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+            <h2 className="text-md md:text-xl font-semibold text-gray-800">Vi kan hjelpe deg med:</h2>
+            <div className="grid grid-cols-3 md:grid-cols-3 gap-2 md:gap-4 mt-4">
                 {prompts.map((prompt, index) => (
                     <button
                         key={index}
-                        className="bg-[#274247] text-white px-4 py-2 shadow-lg text-lg hover:bg-[#1b3134] transition-colors"
+                        className="bg-[#274247] text-white px-2 md:px-4 py-1 md:py-2 shadow-lg text-xs md:text-lg hover:bg-[#1b3134] transition-colors"
                         onClick={() => onSelectPrompt(prompt)}
                     >
                         {prompt}
