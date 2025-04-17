a1f89ee2c2a10fea528f0a0f5493b8e7bb2797ea
Trygve Jørgensen <trygveabjo@gmail.com>
2025-04-17 13:16:04 +0200

    chore(project): fixed build error

--- app/services/parsingRunnableRetryWrapper.ts
+++ app/services/parsingRunnableRetryWrapper.ts
@@ -3,7 +3,7 @@ import {HumanMessage, SystemMessage} from "@langchain/core/messages";
 import {BaseChatModel} from "@langchain/core/language_models/chat_models";
 import {ChatPromptTemplate} from "@langchain/core/prompts";
 
-export async function parsingRunnableRetryWrapper(model: BaseChatModel, userPrompt: string, decoupledRunnable: DecoupledRunnable, maxRetries: number = 5): Promise<any> {
+export async function parsingRunnableRetryWrapper(model: BaseChatModel, userPrompt: string, decoupledRunnable: DecoupledRunnable, maxRetries: number = 5) {
     let attempts = 0;
     let lastError: string = "";
     
